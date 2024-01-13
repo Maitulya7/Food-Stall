c52941731e37b2a22b5dcb99a618428f1135fa6b
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-01-13 11:03:40 +0530

    #AdminRegistration

--- food-stall/app/admin/page.js
+++ food-stall/app/admin/page.js
@@ -13,8 +13,8 @@ const Admin = () => {
               <h1 className='text-3xl font-semibold mb-1'>Welcome back!</h1>
               <p className='text-base mb-8'>Enter your credentials to access your account</p>
 
-              <TextField className='mb-5 w-full' id="outlined-basic" label="Email" variant="outlined" />
-              <TextField className='mb-5 w-full' id="outlined-basic" label="Password" variant="outlined" />
+              <TextField className='mb-5 w-full' id="outlined-basic" label="Email" variant="outlined" type='email' />
+              <TextField className='mb-5 w-full' id="outlined-basic" label="Password" variant="outlined"  type='password'/>
               <button className='bg-green-900 h-12 rounded-lg text-white font-medium w-full'>Login</button>
 
               <div className='flex items-center justify-center mt-5'>
--- food-stall/app/admin/registration/page.js
+++ food-stall/app/admin/registration/page.js
@@ -3,65 +3,82 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const page = () => {
-  return (
-    <>
-     <div className='flex flex-col-reverse md:flex-row justify-between h-screen'>
+    return (
+        <>
+            <div className='flex flex-col-reverse md:flex-row justify-between h-screen'>
 
-<div className='md:h-screen md:w-1/2'>
-  <div className='flex justify-center mt-12 md:mt-52 w-full'>
-    <div className='flex flex-col items-center'>
-      <h1 className='text-3xl font-semibold mb-1'>Welcome back!</h1>
-      <p className='text-base mb-8'>Enter your credentials to access your account</p>
+                <div className='md:h-screen md:w-1/2'>
+                    <div className='flex justify-center mt-12 md:mt-52 w-full'>
+                        <div className='flex flex-col items-center'>
+                            <h1 className='text-3xl font-semibold mb-10'>Admin Registration  </h1>
 
-      <TextField className='mb-5 w-full' id="outlined-basic" label="Email" variant="outlined" />
-      <TextField className='mb-5 w-full' id="outlined-basic" label="Password" variant="outlined" />
-      <button className='bg-green-900 h-12 rounded-lg text-white font-medium w-full'>Login</button>
+                            <TextField
+                                className='mb-5 w-full'
+                                id="outlined-basic"
+                                label="Email"
+                                variant="outlined"
+                                type='email' />
+                            <TextField
+                                className='mb-5 w-full'
+                                id="outlined-basic"
+                                label="Mobile Number"
+                                variant="outlined"
+                                type='number'
+                            />
+                            <TextField
+                                className='mb-5 w-full'
+                                id="outlined-basic"
+                                label="Stall Name"
+                                variant="outlined" 
+                                type='text'/>
 
-      <div className='flex items-center justify-center mt-5'>
-        <div className='w-16 md:w-48 bg-slate-400 h-0.5'></div>
-        <p className='m-3'>Or</p>
-        <div className='w-16 md:w-48 bg-slate-400 h-0.5'></div>
-      </div>
+                            <button className='bg-green-900 h-12 rounded-lg text-white font-medium w-full'>Login</button>
 
-      <div className='flex justify-center items-center mt-10 gap-7'>
-        <div className='flex border border-solid border-slate-400 p-3 rounded-lg hover:cursor-pointer'>
-          <div className='mr-1'>
-            <Image src='/images/google.png' alt='image' width={24} height={24} />
-          </div>
-          <p className='ml-2'>Sign in with Google</p>
-        </div>
-        <div className='flex  border border-solid border-slate-400 p-3 rounded-lg hover:cursor-pointer'>
-          <div className='mr-1'>
-            <Image src='/images/apple.png' alt='image' width={24} height={24} />
-          </div>
-          <p className='ml-2'>Sign in with Apple</p>
-        </div>
-      </div>
+                            <div className='flex items-center justify-center mt-5'>
+                                <div className='w-16 md:w-48 bg-slate-400 h-0.5'></div>
+                                <p className='m-3'>Or</p>
+                                <div className='w-16 md:w-48 bg-slate-400 h-0.5'></div>
+                            </div>
 
-      <p className='flex items-center justify-center mt-10 text-base'>
-        Don’t have an account?   
-        <Link href="/admin/registration " className='ml-1 text-blue-700 font-medium cursor-pointer hover:underline'>
-          Sign Up
-        </Link>
-      </p>
-    </div>
-  </div>
-</div>
+                            <div className='flex justify-center items-center mt-10 gap-7'>
+                                <div className='flex border border-solid border-slate-400 p-3 rounded-lg hover:cursor-pointer'>
+                                    <div className='mr-1'>
+                                        <Image src='/images/google.png' alt='image' width={24} height={24} />
+                                    </div>
+                                    <p className='ml-2'>Sign in with Google</p>
+                                </div>
+                                <div className='flex  border border-solid border-slate-400 p-3 rounded-lg hover:cursor-pointer'>
+                                    <div className='mr-1'>
+                                        <Image src='/images/apple.png' alt='image' width={24} height={24} />
+                                    </div>
+                                    <p className='ml-2'>Sign in with Apple</p>
+                                </div>
+                            </div>
 
-{/* Image Section (Hidden on Small Screens) */}
-<div className='hidden md:block h-screen w-1/2 relative'>
-  <Image
-    src="/images/bg-admin.jpg"
-    alt="Description of the image"
-    layout="fill"
-    objectFit="cover"
-    className='w-full h-full'
-  />
-</div>
+                            <p className='flex items-center justify-center mt-10 text-base'>
+                                Don’t have an account?
+                                <Link href="/admin/registration " className='ml-1 text-blue-700 font-medium cursor-pointer hover:underline'>
+                                    Sign Up
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
-</div>
-    </>
-  )
+                {/* Image Section (Hidden on Small Screens) */}
+                <div className='hidden md:block h-screen w-1/2 relative'>
+                    <Image
+                        src="/images/bg-admin-registration.jpg"
+                        alt="Description of the image"
+                        layout="fill"
+                        objectFit="cover"
+                        className='w-full h-full'
+                    />
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default page
