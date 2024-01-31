d1d345a93e4e31367eef87093e52f7dfeda93967
Sai1710 <voonnasaikiriti22@gmail.com>
2024-01-31 19:03:17 +0530

    Stall card

---
+++ app/components/StallCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <Card className="py-4 m-2">
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="/images/Domino.png"
+          width={270}
+        />
+      </CardBody>
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <button className="bg-green-900 h-8 rounded-lg text-white font-medium w-full">
+          View
+        </button>
+      </CardHeader>
+    </Card>
+  );
+}
