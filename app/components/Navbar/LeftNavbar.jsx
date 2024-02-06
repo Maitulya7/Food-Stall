1ff8ee0e8d6868ae4035917d1475ad03e7dd1194
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-02-06 21:57:02 +0530

    #2 adminDashboard

--- app/components/Navbar/LeftNavbar.jsx
+++ app/components/Navbar/LeftNavbar.jsx
@@ -1,92 +1,34 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+import MenuItem from "./MenuItem";
 
 const LeftNavbar = () => {
   return (
-    <div >
-      <div className="bg-white w-full pt-1 ">
-        <div className="flex gap-2 items-center ml-8 mt-10 mb-14">
-          <Image
-            src="/images/admin-logo.png"
-            alt="logo"
-            width={36}
-            height={36}
-          />
-          <h1 className="text-2xl font-semibold text-green-600">FoodM</h1>
-        </div>
+    <div className="bg-white w-full pt-1 h-auto rounded-xl">
+      <div className="flex items-center ml-8 mt-10 mb-14">
+        <Image src="/images/admin-logo.png" alt="logo" width={36} height={36} />
+        <h1 className="text-2xl font-semibold text-green-600">FoodM</h1>
+      </div>
 
-        <div className="flex flex-col  gap-10 pb-20">
-          <div className="flex gap-4 items-center ml-8 pl-4">
-            <Image src="/images/home.png" alt="logo" width={28} height={28} />
-            <Link
-              href="/admin/dashboard"
-              className="text-base font-medium text-green-600 lg:text-lg "
-            >
-              Home
-            </Link>
-          </div>
-          <div className="flex gap-4 items-center ml-8 pl-4">
-            <Image
-              src="/images/food-order.png"
-              alt="logo"
-              width={28}
-              height={28}
-            />
-            <Link
-              href="/admin/foodOrder"
-              className="text-base font-medium text-green-600 lg:text-lg "
-            >
-              Food Order
-            </Link>
-          </div>
-          <div className="flex gap-4 items-center ml-8 pl-4">
-            <Image src="/images/menu.png" alt="logo" width={28} height={28} />
-            <Link
-              href="/admin/menu"
-              className="text-base font-medium text-green-600 lg:text-lg "
-            >
-              Menu
-            </Link>
-          </div>
-          <div className="flex gap-4 items-center ml-8 pl-4">
-            <Image src="/images/bills.png" alt="logo" width={28} height={28} />
-            <Link
-              href="/admin/bills"
-              className="text-base font-medium text-green-600 lg:text-lg "
-            >
-              Bills
-            </Link>
-          </div>
-          <div className="flex gap-4 items-center ml-8 pl-4">
-            <Image
-              src="/images/settings.png"
-              alt="logo"
-              width={28}
-              height={28}
-            />
-            <Link
-              href="/admin/setting"
-              className="text-base font-medium text-green-600 lg:text-lg "
-            >
-              Setting
-            </Link>
-          </div>
-          <div className="flex gap-4 items-center ml-8 pl-4">
-            <Image
-              src="/images/edit-profile.png"
-              alt="logo"
-              width={28}
-              height={28}
-            />
-            <Link
-              href="/admin/editProfile"
-              className="text-base font-medium text-green-600 lg:text-lg "
-            >
-              Edit Profile
-            </Link>
-          </div>
-        </div>
+      <div className="flex flex-col gap-10 pb-20">
+        <MenuItem icon="/images/home.png" href="/admin/dashboard">
+          Home
+        </MenuItem>
+        <MenuItem icon="/images/food-order.png" href="/admin/foodOrder">
+          Food Order
+        </MenuItem>
+        <MenuItem icon="/images/menu.png" href="/admin/menu">
+          Menu
+        </MenuItem>
+        <MenuItem icon="/images/bills.png" href="/admin/bills">
+          Bills
+        </MenuItem>
+        <MenuItem icon="/images/settings.png" href="/admin/setting">
+          Setting
+        </MenuItem>
+        <MenuItem icon="/images/edit-profile.png" href="/admin/editProfile">
+          Edit Profile
+        </MenuItem>
       </div>
     </div>
   );
---
+++ app/components/Navbar/MenuItem.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+const MenuItem = ({ icon, href, children }) => (
+    <div className="flex gap-4 items-center ml-8 pl-4">
+      <Image src={icon} alt="logo" width={28} height={28} />
+      <Link href={href} className="text-base font-medium text-green-600 lg:text-lg">
+        {children}
+      </Link>
+    </div>
+  );
+  
+  export default MenuItem;
+  
