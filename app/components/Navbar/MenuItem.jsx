780060f2d551b9ae8af8d8b8e87b1596abe3a530
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-02-07 22:46:45 +0530

    category

--- app/components/Navbar/MenuItem.jsx
+++ app/components/Navbar/MenuItem.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+
 const MenuItem = ({ icon, href, children }) => (
-    <div className="flex gap-4 items-center ml-8 pl-2">
-      <Image src={icon} alt="logo" width={22} height={22} />
-      <Link href={href} className="text-base font-medium text-green-600 lg:text-sm">
-        {children}
-      </Link>
-    </div>
-  );
-  
-  export default MenuItem;
-  
+  <div className="flex gap-4 items-center ml-8 pl-2">
+    <Image src={icon} alt="logo" width={22} height={22} />
+    <Link href={href} className="text-base font-medium text-green-600 lg:text-sm">
+      {children}
+    </Link>
+  </div>
+);
+
+export default MenuItem;
