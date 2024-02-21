f11d54b3f139aa5d9a6ff8cac38f758e0e601e24
Maitulya7 <vaghelamaitulya85@gmail.com>
2024-02-21 18:30:42 +0530

    edit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-const DEFAULT_URL = "https://3ee8-2401-4900-1f3f-bc4c-6492-45f2-2c04-5ae3.ngrok-free.app/";
+const DEFAULT_URL = "https://6b95-2401-4900-1f3f-c980-8ead-6c63-5d76-de83.ngrok-free.app";
 
 export default DEFAULT_URL;
